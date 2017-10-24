54ab5279aa6c38bed4df7dc01118991770eb93bd
Razmik Bourtoutian <razmik1984@gmail.com>
2017-10-24 21:52:43 +0400

    Create api-answers.js

---
+++ src/api/api-answers.js
@@ -0,0 +1,67 @@
+import germany from '../assets/germany.png';
+import brazil from '../assets/brazil.png';
+import italy from '../assets/italy.png';
+import buffon from '../assets/buffon.png';
+import abbiati from '../assets/abbiati.png';
+import zoff from '../assets/zoff.png';
+import zlatan from '../assets/zlatan.png';
+import owen from '../assets/owen.png';
+import gerrard from '../assets/gerrard.png';
+
+export const answers = [
+    {
+      id: 1,
+      name: "Germany",
+      image: germany,
+      description: true
+    },
+    {
+      id: 2,
+      name: "Brazil",
+      image: brazil,
+      description: false
+    },
+    {
+      id: 3,
+      name: "Italy",
+      image: italy,
+      description: false
+    },
+    {
+      id: 4,
+      name: "Dino Zoff",
+      image: zoff,
+      description: false
+    },
+    {
+      id: 5,
+      name: "Christian Abbiati",
+      image: abbiati,
+      description: false
+    },
+    {
+      id: 6,
+      name: "Gianluigi Buffon",
+      image: buffon,
+      description: true
+    },
+    {
+      id: 7,
+      name: "Michael Owen ",
+      image: owen,
+      description: true
+    },
+    {
+      id: 8,
+      name: "Zlatan Ibrahimovic",
+      image: zlatan,
+      description: false
+    },
+    {
+      id: 9,
+      name: "Steven Gerrard",
+      image: gerrard,
+      description: false
+    }
+  ];
+
