046734a888784ff52d9501b7b0bbb1bde5fa4b51
Razmik Bourtoutian <razmik1984@gmail.com>
2017-10-24 21:54:44 +0400

    Add files via upload

---
+++ src/api/api-questions.js
@@ -0,0 +1,14 @@
+export const questions = [
+    {
+        id: 1,
+        question: "Which country won World Cup 1990 ?"
+    },
+    {
+        id: 2,
+        question: "Who was Italy's goalkeeper in World Cup 1998 ?"
+    },
+    {
+        id: 3,
+        question: "Who is the only player to have won silverware at both Manchester United and Liverpool ?"
+    }
+]
