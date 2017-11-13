76e1d5780248951e3a95f55f54bb9b4d427768f1
Razmik Bourtoutian <razmik1984@gmail.com>
2017-11-13 19:10:30 +0400

    Update api-questions.js

--- src/api/api-questions.js
+++ src/api/api-questions.js
@@ -9,6 +9,37 @@ export const questions = [
     },
     {
         id: 3,
-        question: "Who is the only player to have won silverware at both Manchester United and Liverpool ?"
+        question: `Who is the player to have won 
+                   silverware at Man. United and Liverpool ?`
+    },
+    {
+        id: 4,
+        question: "Which team won most uefa Champions League titles ?"
+    },
+    {
+        id: 5,
+        question: "Who is the all time World Cup scorer ?"
+    },
+    {
+        id: 6,
+        question: "Which country will host fifa World Cup in 2022 ?"
+    },
+    {
+        id: 7,
+        question: "Who was fifa player of the year 2001 ?"
+    },
+    {
+        id: 8,
+        question: "What country has participated in every World Cup?"
+    },
+    {
+        id: 9,
+        question: "Which country won World Cup 1982 ?"
+    },
+    {
+        id: 10,
+        question: "Which club has the most trophies in Europe ?"
     }
+    
 ]
+
