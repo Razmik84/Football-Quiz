9ad5c0753703bae35a90b5dc043e9d0fe516e10b
Razmik Bourtoutian <razmik1984@gmail.com>
2017-11-13 19:09:47 +0400

    Update api-answers.js

--- src/api/api-answers.js
+++ src/api/api-answers.js
@@ -7,6 +7,24 @@ import zoff from '../assets/zoff.png';
 import zlatan from '../assets/zlatan.png';
 import owen from '../assets/owen.png';
 import gerrard from '../assets/gerrard.png';
+import real from '../assets/real.png';
+import milan from '../assets/milan.png';
+import bayern from '../assets/bayern.png';
+import pele from '../assets/pele.jpg';
+import ronaldo from '../assets/ronaldo.jpg';
+import klose from '../assets/klose.jpg';
+import russia from '../assets/russia.jpg';
+import qatar from '../assets/qatar.jpg';
+import usa from '../assets/usa.png';
+import zidane from '../assets/zidane.jpg';
+import cRonaldo from '../assets/cronaldo.jpg';
+import figo from '../assets/figo.jpg';
+import germanyFlag from '../assets/germanyFlag.png';
+import brazilFlag from '../assets/brazilFlag.png';
+import italyFlag from '../assets/italyFlag.jpg';
+import uruguay from '../assets/uruguay.png';
+import barcelona from '../assets/barcelona.png';
+import juventus from '../assets/juventus.jpg';
 
 export const answers = [
     {
@@ -62,6 +80,134 @@ export const answers = [
       name: "Steven Gerrard",
       image: gerrard,
       description: false
+    },
+    {
+      id: 10,
+      name: "Bayern Munich",
+      image: bayern,
+      description: false
+    },
+    {
+      id: 11,
+      name: "AC Milan",
+      image: milan,
+      description: false
+    },
+    {
+      id: 12,
+      name: "Real Madrid",
+      image: real,
+      description: true
+    },
+    {
+      id: 13,
+      name: "Ronaldo",
+      image: ronaldo,
+      description: false
+    },
+    {
+      id: 14,
+      name: "Miroslav Klose",
+      image: klose,
+      description: true
+    },
+    {
+      id: 15,
+      name: "Pele",
+      image: pele,
+      description: false
+    },
+    {
+      id: 16,
+      name: "Qatar",
+      image: qatar,
+      description: true
+    },
+    {
+      id: 17,
+      name: "Russia",
+      image: russia,
+      description: false
+    },
+    {
+      id: 18,
+      name: "USA",
+      image: usa,
+      description: false
+    },
+    {
+      id: 19,
+      name: "Luis Figo",
+      image: figo,
+      description: true
+    },
+    {
+      id: 20,
+      name: "Zidane",
+      image: zidane,
+      description: false
+    },
+    {
+      id: 21,
+      name: "Cristiano Ronaldo",
+      image: cRonaldo,
+      description: false
+    },
+    {
+      id: 22,
+      name: "Germany",
+      image: germanyFlag,
+      description: false
+    },
+    {
+      id: 23,
+      name: "Brazil",
+      image: brazilFlag,
+      description: true
+    },
+    {
+      id: 24,
+      name: "Italy",
+      image: italyFlag,
+      description: false
+    },
+    {
+      id: 25,
+      name: "Germany",
+      image: germanyFlag,
+      description: false
+    },
+    {
+      id: 26,
+      name: "Uruguay",
+      image: uruguay,
+      description: true
+    },
+    {
+      id: 27,
+      name: "Italy",
+      image: italyFlag,
+      description: true
+    },
+    {
+      id: 28,
+      name: "Barcelona",
+      image: barcelona,
+      description: true
+    },
+    {
+      id: 29,
+      name: "Juventus",
+      image: juventus,
+      description: false
+    },
+    {
+      id: 30,
+      name: "Bayern Munich",
+      image: bayern,
+      description: false
     }
+    
   ];
+
 
